924aa6f158ab7a9ef49705a2517b2737f7c4ab3b
Juan A. Rodríguez <84048520+jrodriguezru@users.noreply.github.com>
2024-12-09 16:56:37 -0500

    App3 (part5)

---
+++ Clima/lib/services/weather.dart
@@ -0,0 +1,71 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '44b515f2534a114827f1d41ba5af7b7c';
+
+class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocationCheckingPermissions();
+    var url = Uri.https(
+      'api.openweathermap.org',
+      'data/2.5/weather',
+      {
+        'lat': location.latitude.toString(),
+        'lon': location.longitude.toString(),
+        'appid': apiKey,
+        'units': 'metric',
+      },
+    );
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = Uri.https(
+      'api.openweathermap.org',
+      'data/2.5/weather',
+      {
+        'q': cityName,
+        'appid': apiKey,
+        'units': 'metric',
+      },
+    );
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
